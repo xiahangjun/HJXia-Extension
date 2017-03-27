ed6813d67407a48f67c553e0f4869c1e109f6112
夏航军 <xiahangjun@9188.com>
2017-03-27 13:57:08 +0800

    commit

---
+++ HJXia-Extension/HJXia-Extension/Classes/Base/BaseViewController/HJBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  HJBaseViewController.h
+//  HJXia-Extension
+//
+//  Created by a-375 on 2017/3/27.
+//  Copyright © 2017年 a-375. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJBaseViewController : UIViewController
+
+@end
---
+++ HJXia-Extension/HJXia-Extension/Classes/Base/BaseViewController/HJBaseViewController.m
@@ -0,0 +1,37 @@
+//
+//  HJBaseViewController.m
+//  HJXia-Extension
+//
+//  Created by a-375 on 2017/3/27.
+//  Copyright © 2017年 a-375. All rights reserved.
+//
+
+#import "HJBaseViewController.h"
+
+@interface HJBaseViewController ()
+
+@end
+
+@implementation HJBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
